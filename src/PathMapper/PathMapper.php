2f466f90c4fa7b38ec63c321fb3fe6371def1e84
dannylamb <dlamb@islandora.ca>
2017-04-13 11:25:33 -0300

    Path mapper (#6)
    * Repo setup.  Adding FedoraResourceConverter
    
    * License, readme, all that jazz.
    
    * PR template
    
    * Resonding to @ruebot's feedback
    
    * Triggering travis
    
    * Ignoring .git and vendor dirs for line endings check.
    
    * Getting rid of .scripts
    
    * Tests for the non-one-liners and touched up a few doc comments.

---
+++ src/PathMapper/PathMapper.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace Islandora\Crayfish\Commons\PathMapper;
+
+use Doctrine\DBAL\Connection;
+
+/**
+ * Class PathMapper
+ * @package Islandora\Crayfish\Commons
+ */
+class PathMapper implements PathMapperInterface
+{
+
+    /**
+     * @var \Doctrine\DBAL\Connection
+     */
+    protected $connection;
+
+    /**
+     * PathMapper constructor.
+     * @param \Doctrine\DBAL\Connection $connection
+     */
+    public function __construct(Connection $connection)
+    {
+        $this->connection = $connection;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getFedoraPath($drupal_path)
+    {
+        $sql = "SELECT fedora FROM Gemini WHERE drupal = :path";
+        $stmt = $this->connection->executeQuery(
+            $sql,
+            ['path' => $drupal_path]
+        );
+        $result = $stmt->fetch();
+
+        if (isset($result['fedora'])) {
+            return $result['fedora'];
+        }
+
+        return null;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getDrupalPath($fedora_path)
+    {
+        $sql = "SELECT drupal FROM Gemini WHERE fedora = :path";
+        $stmt = $this->connection->executeQuery(
+            $sql,
+            ['path' => $fedora_path]
+        );
+        $result = $stmt->fetch();
+
+        if (isset($result['drupal'])) {
+            return $result['drupal'];
+        }
+
+        return null;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function createPair($drupal_path, $fedora_path)
+    {
+        $this->connection->insert(
+            'Gemini',
+            ['drupal' => $drupal_path, 'fedora' => $fedora_path]
+        );
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function deleteFromDrupalPath($drupal_path)
+    {
+        return $this->connection->delete(
+            'Gemini',
+            ['drupal' => $drupal_path]
+        );
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function deleteFromFedoraPath($fedora_path)
+    {
+        return $this->connection->delete(
+            'Gemini',
+            ['fedora' => $fedora_path]
+        );
+    }
+}
