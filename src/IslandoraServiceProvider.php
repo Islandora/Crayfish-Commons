e79b22b0135f7b463e9a821d8e77e8e695232c5a
Jonathan Green <jonathangreen@users.noreply.github.com>
2017-04-13 12:33:58 -0300

    Add a new Symfony securty guard (#5)
    * Add a new Symfony securty guard
    
    A new guard that we can use in Silex that should have the same behavior
    as the java Syn code, so we can secure our microservices in the same way
    and use the same tokens everywhere.
    
    * Fix code sniffer issues.
    
    * Fix PHP 7.1 issue in tests.
    
    * Fix coding standards issue in test.
    
    * Add code coverage to this repo.
    
    * Add a Islandora service provider
    
    * Code style fix.
    
    * Update composer lock file
    
    * Update based on code review.

---
+++ src/IslandoraServiceProvider.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace Islandora\Crayfish\Commons;
+
+use Pimple\Container;
+use Pimple\ServiceProviderInterface;
+use Monolog\Logger;
+use Monolog\Handler\NullHandler;
+use Silex\Provider\MonologServiceProvider;
+use Silex\Provider\ServiceControllerServiceProvider;
+use Silex\Provider\SecurityServiceProvider;
+use Islandora\Chullo\FedoraApi;
+use Islandora\Crayfish\Commons\Syn\SettingsParser;
+use Islandora\Crayfish\Commons\Syn\JwtAuthenticator;
+use Islandora\Crayfish\Commons\Syn\JwtFactory;
+
+class IslandoraServiceProvider implements ServiceProviderInterface
+{
+    protected $config;
+
+    public function __construct($config)
+    {
+        $this->config = $config;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function register(Container $app)
+    {
+        $config = $this->config;
+
+        // Only use logger if configured
+        if (strtolower($config['loglevel']) === 'none') {
+            $app['monolog'] = function () {
+                return new Logger('null', [new NullHandler()]);
+            };
+        } else {
+            $app->register(new MonologServiceProvider(), [
+                'monolog.logfile' => $config['logfile'],
+                'monolog.level' => $config['loglevel'],
+            ]);
+        }
+
+        $app->register(new ServiceControllerServiceProvider());
+
+        $app['crayfish.cmd_execute_service'] = function ($app) {
+            return new CmdExecuteService(
+                $app['monolog']->withName('crayfish.cmd_execute_service')
+            );
+        };
+
+        $app['crayfish.fedora_resource'] = function () use ($config) {
+            return new FedoraResourceConverter(
+                FedoraApi::create($config['fedora base url'])
+            );
+        };
+
+        $app['crayfish.syn.settings_parser'] = function ($app) use ($config) {
+            $xml = file_get_contents($config['security config']);
+            return new SettingsParser(
+                $xml,
+                $app['monolog']->withName('crayfish.syn.settings_parser')
+            );
+        };
+
+        $app['crayfish.syn.jwt_authentication'] = function ($app) {
+            return new JwtAuthenticator(
+                $app['crayfish.syn.settings_parser'],
+                new JwtFactory(),
+                $app['monolog']->withName('crayfish.syn.jwt_authentication')
+            );
+        };
+
+        if ($config['security enabled']) {
+            $app->register(new SecurityServiceProvider());
+            $app['security.firewalls'] = [
+                'default' => [
+                    'stateless' => true,
+                    'anonymous' => false,
+                    'guard' => [
+                        'authenticators' => [
+                            'crayfish.syn.jwt_authentication'
+                        ],
+                    ],
+                ],
+            ];
+        }
+    }
+}
