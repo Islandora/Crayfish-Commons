4678e13e988a2e360d76bf542e35d3c291dbeb56
Jonathan Green <jonathan@razzard.com>
2017-09-18 17:20:31 -0300

    Add some tests to new parsing options.

--- tests/Provider/IslandoraServiceProviderTest.php
+++ tests/Provider/IslandoraServiceProviderTest.php
@@ -31,11 +31,17 @@ class IslandoraServiceProviderTest extends PHPUnit_Framework_TestCase
         $this->assertInstanceOf(Logger::class, $this->container['monolog']);
     }
 
+    public function testDoctrineOptions()
+    {
+        $this->container['crayfish.db.options.foo'] = 'bar';
+        $this->assertArrayHasKey('foo', $this->container['db.options']);
+        $this->assertEquals('bar', $this->container['db.options']['foo']);
+    }
+
     public function testDoctrine()
     {
         // doctrine uses logger
         $this->container['crayfish.log.level'] = 'none';
-
         $this->assertInstanceOf(Connection::class, $this->container['db']);
     }
 
