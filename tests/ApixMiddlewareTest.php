9e0ec7f82435c8ef97c47121a2faa5da6eae72af
Jared Whiklo <jwhiklo@gmail.com>
2020-12-29 09:47:11 -0600

    Correct namespace for tests (#39)
    Restrict symfony versions
    
    Fix deprecation in test

--- tests/ApixMiddlewareTest.php
+++ tests/ApixMiddlewareTest.php
@@ -13,8 +13,17 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\Event\RequestEvent;
 use Symfony\Component\HttpKernel\HttpKernelInterface;
 
+/**
+ * Class ApixMiddlewareTest
+ * @package Islandora\Crayfish\Commons\tests
+ * @coversDefaultClass \Islandora\Crayfish\Commons\ApixMiddleware
+ */
 class ApixMiddlewareTest extends TestCase
 {
+    /**
+     * @covers ::before
+     * @covers ::getFedoraResource
+     */
     public function testReturnsFedoraError()
     {
         $prophecy = $this->prophesize(HttpKernelInterface::class);
@@ -68,6 +77,10 @@ class ApixMiddlewareTest extends TestCase
         );
     }
 
+    /**
+     * @covers ::before
+     * @covers ::getFedoraResource
+     */
     public function testReturns400IfNoApixLdpResourceHeader()
     {
         $prophecy = $this->prophesize(HttpKernelInterface::class);
